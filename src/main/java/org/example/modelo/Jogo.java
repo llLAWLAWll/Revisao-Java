6a63d85c5ed531b1a748d8093f98b8c409118bc6
llLAWLAWll <lucasgs2218@gmail.com>
2024-08-29 23:07:32 -0300

    [feat]  - Jogo
     Implementação da classe Jogo com os atributos
    
     - titulo;
     - classificacaoEtaria;
     - preco;

--- src/main/java/org/example/modelo/Jogo.java
+++ src/main/java/org/example/modelo/Jogo.java
@@ -1,4 +1,15 @@
 package org.example.modelo;
 
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class Jogo {
+    private String titulo;
+    private Integer classificacaoEtaria;
+    private Double preco;
+
 }
